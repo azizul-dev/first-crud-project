f2e87fb941d397f161616df2b4af31a3423c1c41
Md Azizul Islam <mdazizulislam@Mds-MacBook-Air.local>
2026-05-10 00:43:02 +0600

    navbar, banner and footer added

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,49 @@
+import { Button } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+        <div className=" container mx-auto py-10">
+      <nav className=" flex justify-between items-center">
+        <ul className=" flex gap-3">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/destinations"}>Destinations</Link>
+          </li>
+          <li>
+            <Link href={"/my-bookings"}>My Bookings</Link>
+          </li>
+        </ul>
+
+        <div>
+          <Image
+            src={"/assets/Wanderlast.png"}
+            alt="Wanderlat"
+            height={200}
+            width={200}
+          />
+        </div>
+
+        <ul className=" flex gap-3">
+          <li>
+            <Link href={"/profile"}>Profile</Link>
+          </li>
+          <li>
+            <Link href={"/login"}>Login</Link>
+          </li>
+          <li>
+            <Link href={"/signup"}>SignUp</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    </div>
+  );
+};
+
+export default NavBar;
